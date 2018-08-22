ec43c7b59ab9d979dc33002baa167f1783d7882f
mack-h <35611535+mack-h@users.noreply.github.com>
2018-08-22 13:58:48 -0700

    Add files via upload

--- np_report_parser_modA.py
+++ np_report_parser_modA.py
@@ -1,13 +1,17 @@
 import glob
+import shutil
 import re
 from typing import List, Union
 
 fileList_working = []
 
-def get_files():
+def get_files(spec = 0):
 
     #excluded_files = []
-    fileList_0 = sorted(glob.glob('R:/groups/seeley_pathology/NP Reports/Finalized/*.doc*'))
+    if spec == 0:
+        fileList_0 = sorted(glob.glob('R:/groups/seeley_pathology/NP Reports/Finalized/*.doc*'))
+    else:
+        fileList_0 = spec
     fileList = [f for f in fileList_0 if not '$' in f]
     #print(excluded_files)
     #print(len(excluded_files))
@@ -15,24 +19,18 @@ def get_files():
     return fileList
 
 """
-    for filename in fileList:
-
-        if 'NP' not in filename:
-            excluded_files.append(filename)
-            fileList.remove(filename)
-"""
-        #else:
-            #print(filename)
-
+print('\nResults of get_files:')
 for filename in get_files():
     print(filename)
 print(len(get_files()))
-
-
+"""
 
 def get_pNums(spec=0):
+    if spec == 0:
+        fileList = get_files()
+    else:
+        fileList = spec
 
-    fileList = get_files()
     pNum_list= []
 
     dup_pNums = []
@@ -52,73 +50,117 @@ def get_pNums(spec=0):
     else:
         return sorted(pNum_list)
 
+"""
+print('\nResults of get_pNums:')
 print(get_pNums())
 print(len(get_pNums()))
+"""
 
-def get_dup_pNums():
+def get_dup_pNums(spec = 0):
 
-    dup_pNums = get_pNums('duplicates')
+    pNum_list = []
+    dup_pNums = []
+
+    if spec == 0:
+        fileList = get_files()
+    else:
+        fileList = spec
+
+    for filename in fileList:
+        f = filename.split('_')
+        f = f[3]
+        pNum = f[:5]
+
+        if pNum in pNum_list:
+            dup_pNums.append(pNum)
+        else:
+            pNum_list.append(pNum)
 
     return dup_pNums
 
-
-
+"""
+print('\nResults of get_dup_pNums:')
 for pNum in sorted(get_dup_pNums()):
     print(pNum)
 print(len(get_dup_pNums()))
+"""
 
 
 
+def remove_dup_pNums(spec = 0):
 
-def remove_dup_pNums():
-
-    global fileList_working
-    dup_pNums = get_dup_pNums()
-
-    if not fileList_working:
+    if spec == 0:
         fileList = get_files()
     else:
-        fileList = fileList_working
+        fileList = spec
 
-    for pNum in dup_pNums:
+    #global fileList_working
+    #if not fileList_working:
+    #    fileList = get_files()
+    #else:
+        #fileList = fileList_working
 
-        dup_pNum_files = []
+    dup_pNums = get_dup_pNums(spec)
+    if not dup_pNums:
+        print('\nWorking file list (no duplicate P numbers)')
 
-        for filename in fileList:
-            if pNum in filename:
-                dup_pNum_files.append(filename)
+    else:
+        for pNum in dup_pNums:
 
-        i = 1
-        pNum_choice_numbers = [i]
+            dup_pNum_files = []
 
-        print('Which of these files would you like to keep in the queue?')
+            for filename in fileList:
+                if pNum in filename:
+                    dup_pNum_files.append(filename) # I think the problem is here.
 
-        for filename in dup_pNum_files:
+            i = 1
+            pNum_choice_numbers = [i]
 
-            print(f'{i}: {filename}')
+            print('Which of these files would you like to keep in the queue?')
 
-            i = i+1
-            pNum_choice_numbers.append(i)
+            if len(dup_pNum_files) > 1:
+                for filename in dup_pNum_files:
 
-        usr_choice = int(input('Please type the number of file you would like to keep (on the left):'))
+                    print(f'{i}: {filename}')
 
-        for filename in dup_pNum_files:
-            if dup_pNum_files.index(filename) != (usr_choice - 1):
-                fileList.remove(filename)
+                    i = i+1
+                    pNum_choice_numbers.append(i)
+
+                usr_choice = int(input('Please type the number of file you would like to keep (on the left):'))
+
+                for filename in dup_pNum_files:
+                    if dup_pNum_files.index(filename) != (usr_choice - 1):
+                        fileList.remove(filename)
 
     fileList_working = fileList
+
     return fileList_working
 
+fileList_working_0 = remove_dup_pNums()
+fileList_working = remove_dup_pNums(fileList_working_0)
 
+print('\nResults of remove_dup_pNums (fileList_working_0):')
+for filename in fileList_working_0:
+    print(filename)
+print(len(fileList_working_0))
 
-
-fileList_working = remove_dup_pNums()
+"""
+print('\nResults of remove_dup_pNums (fileList_working):')
 for filename in fileList_working:
     print(filename)
 print(len(fileList_working))
 
+## Okay, this seems chill...
+
+## Let's try and actually get in the files...
+"""
+
+
+#print('Result of get_pNums:')
 #get_pNums()
 #get_dup_pNums()
+#print('Result of remove_dup_pNums:')
 #remove_dup_pNums()
+#print('Result of ')
 #get_files()
 
--- np_report_parser_modB.py
+++ np_report_parser_modB.py
@@ -1,4 +1,6 @@
 import re
+import shutil
+# import win32compat.client as win32
 
 #from np_report_parser_modA import get_files, get_pNums, get_dup_pNums, remove_dup_pNums
 
@@ -6,10 +8,18 @@ import re
 
 import sqlite3
 
+
 #fileList = remove_dup_pNums() # save for later
 
 
-working_file = 'R:\groups\seeley\Mack/NP report parser/_NP_Report_P2817_final.txt'
+working_file = 'R:\groups\seeley\Mack/NP report parser/_NP_Report_P2679_final.txt'
+#table_file = 'R:\groups\seeley\Mack/NP report parser/NP_Report_P2722_final.docx'
+
+
+## Copy files as plain text, store in NP report parser folder
+
+
+
 
 # Okay. First, let's try and generate something resembling the Excel spreadsheet from the NP report.
 
@@ -48,6 +58,9 @@ def open_file():
             lines.append(line)
     return lines
 
+
+
+
 def get_dx_sxn():
 
     lines = open_file()
@@ -55,8 +68,8 @@ def get_dx_sxn():
     for line in lines:
         if 'Final Neuropathological Diagnoses' in line:
             i_1 = lines.index(line)
-        if '*Designations' in line:
-            i_2 = lines.index(line)
+        if 'Clinical History' in line:
+            i_2 = lines.index(line) - 1
 
     dx_line_sxn = lines[i_1:i_2+1]
     dx_line_sxn = [item.upper() for item in dx_line_sxn]
@@ -66,9 +79,30 @@ def get_dx_sxn():
 
 #print(len(get_dx_sxn()))
 #print(type(get_dx_sxn()))
+
+## Testing get_dx_sxn
 for line in get_dx_sxn():
     print(line)
 print(get_dx_sxn())
+
+## Gross obs section
+def get_grossObs():
+    lines = open_file()
+
+    for line in lines:
+        if 'Gross Observations' in line:
+            i_1 = lines.index(line)
+        if 'Other gross findings' in line:
+            i_2 = lines.index(line) + 1
+
+    grossObs = lines[i_1:i_2]
+    grossObs = [item.upper() for item in grossObs]
+
+    return grossObs
+
+## Testing get_grossObs
+for line in get_grossObs():
+    print(line)
 
 def get_PrimDx():
     lines = get_dx_sxn()
@@ -76,8 +110,12 @@ def get_PrimDx():
     primDx_list = []
     for line in lines:
         if 'PRIMARY DIAGNOSIS' in line:
-            dx = line[line.index(':')+2:]
-            primDx_list.append(dx)
+            if ':' in line:
+                dx = line[line.index(':')+2:]
+                primDx_list.append(dx)
+            elif '#' in line:
+                dx = line[line.index('#') + 2:]
+                primDx_list.append(dx)
 
     return primDx_list
 
@@ -94,8 +132,13 @@ def get_ContributingDx():
     contributingDx_list = []
     for line in lines:
         if 'CONTRIBUTING DIAGNOSIS' in line:
-            dx = line[line.index(':')+2:]
-            contributingDx_list.append(dx)
+            if ':' in line:
+                dx = line[line.index(':')+2:]
+                contributingDx_list.append(dx)
+
+            elif '#' in line:
+                dx = line[line.index('#') + 2:]
+                contributingDx_list.append(dx)
 
     return contributingDx_list
 
@@ -113,8 +156,13 @@ def get_IncidentalDx():
 
     for line in lines:
         if 'INCIDENTAL DIAGNOSIS' in line:
-            dx = line[line.index(':')+2:]
-            IncidentalDx_list.append(dx)
+            if ':' in line:
+                dx = line[line.index(':')+2:]
+                IncidentalDx_list.append(dx)
+            elif '#' in line:
+                dx = line[line.index('#') + 3:]
+# This is to account for typos where there is no colon following a number.
+                IncidentalDx_list.append(dx)
 
     return IncidentalDx_list
 
@@ -360,10 +408,132 @@ def get_PDBraak():
     print(f'PD Braak Stage: {PDBraak_value}')
     return PDBraak_value
 
+def get_ATAC():
+
+    ATAC_value = 0
+
+    for line in get_dx_sxn():
+        if 'ARTAG' in line:
+            ATAC_value = 1
+
+    print(f'ATAC_value: {ATAC_value}')
+    return ATAC_value
+
+## Working on CTE -- varies between cases. ASK BILL about what value we should put when no stage is given (ex. P2502)
+
+def get_CTE():
+
+    CTE_value = 0
+    CTE_stages = {'STAGE 0': 0, 'STAGE I': 1, 'STAGE II': 2, 'STAGE III': 3, 'STAGE IV': 4, 'STAGE V': 5, 'STAGE VI': 6, 'NOT CONFORMING': -4, 'NON-CONFORMING': -4, 'NONCONFORMING': -4, 'NOT APPLICABLE': -4}
+
+    for line in get_dx_sxn():
+        if 'CHRONIC TRAUMATIC' in line:
+            if 'STAGE' in line:
+                for stage in CTE_stages.keys():
+                    if stage in line:
+                        CTE_value = CTE_stages[stage]
+# Even though this will always initially change CTE_value to 1, it will update if needed as it goes down the list
+            else:
+                CTE_value = -7
+# Check with Bill about this
+    print(f'CTE value: {CTE_value}')
+    return CTE_value
+
+
+# Hippocampal sclerosis, in progress (need win32com)
+"""
+def get_HS():
+
+    HS_value = 0
+    HS_stages = [0,1,2,3,4,5]
+    HS_laterality = 0
+
+    for line in get_dx_sxn():
+        if 'HIPPOCAMPAL SCLEROSIS' in line:
+            has_HS = True
+
+    if has_HS:
+"""
+
+def get_arterio():
+
+    arterio_value = 0
+
+    for line in get_dx_sxn():
+        if 'ARTERIOLOSCLEROSIS' in line:
+            if 'MILD' in line:
+                arterio_value = 1
+            elif 'MODERATE' in line:
+                arterio_value = 2
+            elif 'SEVERE' in line:
+                arterio_value = 3
+            else:
+                print('Atypical arteriolosclerosis value, check NP report')
+
+    print(f'Arteriolosclerosis value: {arterio_value}')
+    return arterio_value
+
+def get_athero():
+
+    athero_value = 0
+
+    for line in get_grossObs():
+        if 'ATHEROSCLEROSIS' in line:
+            if 'ABSENT' in line:
+                athero_value = 0
+            elif 'MILD' in line:
+                athero_value = 1
+            elif 'MODERATE' in line:
+                athero_value = 2
+            elif 'SEVERE' in line:
+                athero_value = 3
+            else:
+                print('Atypical atherosclerosis value, check NP report')
+
+    print(f'Atherosclerosis value: {athero_value}')
+    return athero_value
+
+def get_AGD():
+
+    AGD_value = 0
+
+    AGD_stages = {'LIMBIC': 1, 'DIFFUSE': 2}
+
+    for line in get_dx_sxn():
+        if 'GRAIN DISEASE' in line:
+            for stage in AGD_stages.keys():
+                if stage in line:
+                    AGD_value = AGD_stages[stage]
+
+    print(f'AGD value: {AGD_value}')
+    return AGD_value
+
+def get_huntington():
+
+    HD_value = 0
+    pat = r'GRADE [0-4]'
+
+    for line in get_dx_sxn():
+        if 'HUNTINGTON' in line:
+            grade = re.findall(pat, line)
+            #print(f'\nHD grade: {grade}\n')
+            if grade:
+                grade = grade[0]
+                HD_value = grade[-1:]
+            else:
+                print('Atypical HD value, check NP report')
+
+    print(f'Huntington value: {HD_value}')
+    return HD_value
 
 print('\n')
 get_ADNC()
 get_CAA()
 get_lbd_stage()
 get_PDBraak()
-
+get_ATAC()
+get_CTE()
+get_arterio()
+get_athero()
+get_AGD()
+get_huntington()
